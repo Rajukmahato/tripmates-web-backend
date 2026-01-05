56e1f748de5921a90ed8c2118f7570f5ff2fe5bd
Rajukmahato <rajukumarsingh2076@.com>
2026-01-05 18:56:38 +0545

    add repositories

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,25 @@
+import { UserModel, IUser } from "../modules/user.model";
+
+export interface IUserRepository {
+    createUser(userData: Partial<IUser>): Promise<IUser>;
+    getUserByPhoneNumber(phoneNumber: string): Promise<IUser | null>;
+    getUserById(userId: string): Promise<IUser | null>;
+}
+
+export class UserRepository implements IUserRepository {
+
+    async getUserById(userId: string): Promise<IUser | null> {
+        const user = await UserModel.findById(userId);
+        return user;
+    }
+
+    async getUserByPhoneNumber(phoneNumber: string): Promise<IUser | null> {
+        const user = await UserModel.findOne({ "phoneNumber": phoneNumber });
+        return user;
+    }
+
+    async createUser(userData: Partial<IUser>): Promise<IUser> {
+        const user = new UserModel(userData);
+        return await user.save();
+    }
+}
