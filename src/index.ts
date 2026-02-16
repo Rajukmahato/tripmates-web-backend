24e4146fed04d8287680902c124b3f5e3c0c9342
Rajukmahato <rajukumarsingh2076@.com>
2026-02-16 12:39:28 +0545

    add integration tests for auth API

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,15 @@
-import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import bodyparser from 'body-parser'
 import { PORT } from "./configs";
 import { connectDb } from "./database/mongodb";
-import authRouters from "./routes/auth.route";
-import userRouters from "./routes/user.route";
-import cors from 'cors';
+import app from './app';
 
 dotenv.config();
-console.log(process.env.PORT);
-
-const app: Application = express();
-
-let corsOptions = {
-    origin: ['http://localhost:3000', "http://localhost:3003"]
-    //List of accepted domain
-}
-app.use(cors(corsOptions));
-app.use(bodyparser.json())
-app.use("/api/auth", authRouters);
-app.use("/api/user", userRouters);
-app.use("/uploads", express.static("uploads"));
-
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, World!")
-});
 
 async function startServer() {
     await connectDb();
 
     app.listen(PORT, () => {
-        console.log(`Sever: http://localhost:${PORT}`)
+        console.log(`Server: http://localhost:${PORT}`)
     })
 }
 
